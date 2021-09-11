c2281febb8f4b4954e5940aa5d95bd8f644b05c1
Alfred Xiao <alfred.xiao@outlook.com>
2021-09-11 20:27:12 +1000

    non-root user mapped as root, PID/fs isolation works!

--- lizrice-rootless/main.go
+++ lizrice-rootless/main.go
@@ -11,6 +11,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("enter a command pls")
 	}
@@ -19,12 +21,12 @@ func main() {
 func run() {
 	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID,
 		UidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
@@ -41,6 +43,21 @@ func run() {
                 },
 	}
 	must(cmd.Run())
+}
+
+func child() {
+        fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
+
+	must(syscall.Chroot("../alpine"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+        cmd := exec.Command(os.Args[2], os.Args[3:]...)
+        cmd.Stdin = os.Stdin
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+	must(cmd.Run())
+	must(syscall.Unmount("proc", 0))
 }
 
 func must(err error) {
